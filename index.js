ded9cf32e2e26d6f682939fec70d996fa98078ab
3laa-96 <3laa.xvx@gmail.com>
2022-12-16 15:31:22 +0200

    v2

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 // Defined Variables
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = cards[0] + cards[1];
+
+let cards = [];
+let sum = 0;
 let message = "";
-// let hasBlackjack = false;  TEMP
-// let isAlive = true;  TEMP
+let hasBlackJack = false;
+let isAlive = false;
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("card-el");
 
 // start game button
 function startGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards.push(firstCard, secondCard);
+  sum = cards[0] + cards[1];
   renderGame();
+  isAlive = true;
 }
 
 // New card button
 function newCard() {
   let Card = getRandomCard();
-  sum += Card;
-  cards.push(Card);
-  renderGame();
+  if (isAlive === true) {
+    sum += Card;
+    cards.push(Card);
+    renderGame();
+  } else if (hasBlackJack === true) {
+    messageEl.innerText = "You already won";
+  } else {
+    messageEl.innerText = "You haven't started teh game yet";
+  }
 }
 
 // Random number generator
@@ -28,8 +38,10 @@ function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
   if (randomNumber >= 11) {
     return 10;
+    // in case the player gets king,queen or jack
   } else if (randomNumber === 1) {
     return 11;
+    // in case the player gets an ace
   } else return randomNumber;
 }
 
@@ -37,13 +49,13 @@ function getRandomCard() {
 function renderGame() {
   if (sum < 21) {
     message = "Do you want another card ?";
-    // hasBlackJack = false;  TEMP
+    hasBlackJack = false;
   } else if (sum === 21) {
     message = "Congrats you got a black jack !";
-    // hasBlackJack = true;  TEMP
+    hasBlackJack = true;
   } else {
     message = "Bust";
-    // isAlive = false;  TEMP
+    isAlive = false;
   }
   messageEl.innerText = message;
 
@@ -57,4 +69,15 @@ function renderGame() {
 // test roll dice function
 function rollDice() {
   return Math.floor(Math.random() * 5 + 1);
+}
+
+// Testing logical operations
+
+let colorIsBlack = true;
+let conditionIsNew = false;
+
+if (colorIsBlack === true && conditionIsNew === true) {
+  console.log("true");
+} else {
+  console.log("false");
 }
